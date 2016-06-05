1205ae51026ab5ba28c90a86053f03759b46a89a
AbbaraoAnvitha <anvitha.2697@gmail.com>
2016-06-05 19:23:06 +0530

    Update 8

---
+++ Project/StayBookish/src/com/bvrit/StayBookish/controller/BuyController.java
@@ -0,0 +1,46 @@
+package com.bvrit.StayBookish.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import com.bvrit.StayBookish.dao.UserDAO;
+
+
+public class BuyController extends HttpServlet {protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		//read username and password
+		HttpSession session = request.getSession(true);
+		String user = (String)session.getAttribute("user");
+		String bid = request.getParameter("bid");
+		if(user!= null){
+			
+			
+			response.sendRedirect("addtocart?bid="+bid+"");
+			//request.getAttribute("Invalid login credentials");
+		}else{
+			
+			response.sendRedirect("loginView.jsp");
+			
+		}
+
+}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
